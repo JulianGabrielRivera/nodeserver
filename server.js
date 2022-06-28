8259f9f64582aae304c41beee5ccb1602c24f1d0
Julian Rivera <julianarevircoins@gmail.com>
2022-06-28 11:02:11 -0400

    route changes

--- server.js
+++ server.js
@@ -20,6 +20,9 @@ const server = http.createServer((request, response) => {
   if (request.url === '/') {
     response.write('Hello, world!');
     response.end();
+  } else if (request.url === '/about') {
+    response.write('My name is Izzy');
+    response.end();
   } else {
     response.statusCode = 404;
     response.write('404 Page');
